feb144cc1569daf2030203a987f23b81b862e73e
eun2343 <eun2343@gmail.com>
2026-07-07 17:50:11 +0200

    Add heroFlow style for FertilityPage with background image and gradient

--- src/pages/FertilityPage.tsx
+++ src/pages/FertilityPage.tsx
@@ -4,6 +4,7 @@ import FertilityHeroSection from "../components/FertilityHeroSection";
 import FertilitySeedSection from "../components/FertilitySeedSection";
 import FertilityCareSection from "../components/FertilityCareSection";
 import { useState } from "react";
+import styles from "./FertilityPage.module.css";
 
 const FertilityPage = () => {
   const [isDescriptionOpen, setIsDescriptionOpen] = useState(false);
@@ -11,11 +12,13 @@ const FertilityPage = () => {
   return (
     <>
       <Navigation logoScale={0.8} barHeightScale={0.8} />
-      <FertilityHeroSection
-        isDescriptionOpen={isDescriptionOpen}
-        onToggleDescription={() => setIsDescriptionOpen((prev) => !prev)}
-      />
-      <FertilitySeedSection isOpen={isDescriptionOpen} />
+      <div className={styles.heroFlow}>
+        <FertilityHeroSection
+          isDescriptionOpen={isDescriptionOpen}
+          onToggleDescription={() => setIsDescriptionOpen((prev) => !prev)}
+        />
+        <FertilitySeedSection isOpen={isDescriptionOpen} />
+      </div>
       <FertilityCareSection />
       <Footer />
     </>
